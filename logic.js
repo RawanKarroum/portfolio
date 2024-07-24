dc0df3badb10696633ae53dd629831767931710e
Rawan Karroum <rawan.r.karroum@gmail.com>
2024-07-23 21:43:33 -0400

    finished navbar, header, and skills section

---
+++ logic.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+	strings: ["Software Engineer","Full-Stack Developer","Programmer", "Dreamer" ],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+})
