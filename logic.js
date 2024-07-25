43bf2c738a833cd90814defc90e9f569756e5f75
Rawan Karroum <rawan.r.karroum@gmail.com>
2024-07-25 14:06:01 -0400

    fixed skills and project section

--- logic.js
+++ logic.js
@@ -5,3 +5,27 @@ var typed = new Typed(".multiple-text", {
 	backDelay: 1000,
 	loop: true
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const skillItems = document.querySelectorAll('.skill-item');
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                setTimeout(() => {
+                    entry.target.classList.add('visible');
+                }, Math.random() * 1000); // Delay between 0 to 1 second
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    skillItems.forEach(item => {
+        observer.observe(item);
+    });
+});
